a2b636adb4110a8ba971cf224bec0dddaee4072f
DorinVieru <dorino.vieru@gmail.com>
2023-12-04 14:56:29 +0100

    [UPDATE] Complete milestone 0

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// ----- MILESTONE 0 -----
+// CREO UN ARRAY DI OGGETTI INSERENDO LE INFORMAZIONE CONTENENTI NEL PNG "tabell_dipendenti"
+let dipendenti = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+console.log(dipendenti);
+
+
+// ----- MILESTONE 1 -----
+// STAMPO SU CONSOLE LE INFORMAZIONI DI OGNI MEMBRO DEL TEMA
+
+
+
+// ----- MILESTONE 2 -----
+// STAMPO LE MEDISIME INFORMAZIONI DI OGNI MEMEBRO NEL DOM
